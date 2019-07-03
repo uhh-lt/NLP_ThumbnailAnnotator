5c85355440886b767ff0aa61cc1c8f57c79b8b1d
floschne <florian.schneider.1992@gmx.de>
2019-07-03 18:36:43 +0200

    added custum conversions for SentenceContext to RedisConfig

--- thumbnailAnnotator.parent/thumbnailAnnotator.db/src/main/java/nlp/floschne/thumbnailAnnotator/db/RedisConfig.java
+++ thumbnailAnnotator.parent/thumbnailAnnotator.db/src/main/java/nlp/floschne/thumbnailAnnotator/db/RedisConfig.java
@@ -1,21 +1,69 @@
 package nlp.floschne.thumbnailAnnotator.db;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nlp.floschne.thumbnailAnnotator.core.domain.SentenceContext;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.convert.RedisCustomConversions;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 @Configuration
 @EnableRedisRepositories(basePackages = "nlp.floschne.thumbnailAnnotator.db")
+@ComponentScan
 public class RedisConfig {
+
+    @Component
+    @ReadingConverter
+    public class BytesToSentenceContextConverter implements Converter<byte[], SentenceContext> {
+        @Override
+        public SentenceContext convert(final byte[] source) {
+            ObjectMapper jsonMapper = new ObjectMapper();
+            String json = new String(source);
+            try {
+                return jsonMapper.readValue(json, SentenceContext.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    @Component
+    @WritingConverter
+    public class SentenceContextToByteConverter implements Converter<SentenceContext, byte[]> {
+        @Override
+        public byte[] convert(final SentenceContext source) {
+            ObjectMapper jsonMapper = new ObjectMapper();
+            try {
+                String json = jsonMapper.writeValueAsString(source);
+                return json.getBytes();
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    @Bean
+    public RedisCustomConversions redisCustomConversions(BytesToSentenceContextConverter bytesToContext,
+                                                         SentenceContextToByteConverter contextToByes) {
+        return new RedisCustomConversions(Arrays.asList(bytesToContext, contextToByes));
+    }
+
     @Bean
     @Profile("local")
     RedisConnectionFactory connectionFactoryLocal() {
