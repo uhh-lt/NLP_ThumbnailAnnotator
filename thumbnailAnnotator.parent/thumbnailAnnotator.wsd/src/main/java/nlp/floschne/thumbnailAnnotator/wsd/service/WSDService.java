50ed813259d06f1b2bc5fc0ac65af9534dba55ba
floschne <florian.schneider.1992@gmx.de>
2019-07-03 18:47:35 +0200

    fixed serializing and deserializing for NaiveBayesModel

--- thumbnailAnnotator.parent/thumbnailAnnotator.wsd/src/main/java/nlp/floschne/thumbnailAnnotator/wsd/service/WSDService.java
+++ thumbnailAnnotator.parent/thumbnailAnnotator.wsd/src/main/java/nlp/floschne/thumbnailAnnotator/wsd/service/WSDService.java
@@ -7,14 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import nlp.floschne.thumbnailAnnotator.core.domain.CaptionToken;
 import nlp.floschne.thumbnailAnnotator.core.domain.SentenceContext;
 import nlp.floschne.thumbnailAnnotator.core.domain.Thumbnail;
-import nlp.floschne.thumbnailAnnotator.wsd.classifier.IClassifier;
-import nlp.floschne.thumbnailAnnotator.wsd.classifier.Label;
-import nlp.floschne.thumbnailAnnotator.wsd.classifier.NaiveBayesModel;
-import nlp.floschne.thumbnailAnnotator.wsd.classifier.Prediction;
+import nlp.floschne.thumbnailAnnotator.wsd.classifier.*;
 import nlp.floschne.thumbnailAnnotator.wsd.featureExtractor.FeatureVector;
 import nlp.floschne.thumbnailAnnotator.wsd.featureExtractor.IFeatureExtractor;
 import nlp.floschne.thumbnailAnnotator.wsd.featureExtractor.IFeatureVector;
 import nlp.floschne.thumbnailAnnotator.wsd.featureExtractor.MyTestFeatureVector;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,14 +20,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 @Service
 @Slf4j
 public class WSDService {
+    private static final String DEFAULT_MODEL = "/tmp/thumbnailAnnotator/models/defaultModel.bin";
+
     private final Kryo kryo;
 
     private final IFeatureExtractor featureExtractor;
@@ -38,11 +35,20 @@ public class WSDService {
 
     @Autowired
     public WSDService(IFeatureExtractor featureExtractor, IClassifier classifier) {
+        this.kryo = initKryo();
         this.featureExtractor = featureExtractor;
         this.classifier = classifier;
-        this.kryo = initKryo();
+        this.classifier.setModel(initDefaultModel());
 
         log.info("WSD Service ready!");
+    }
+
+    private IModel initDefaultModel() {
+        try {
+            return this.deserializeNaiveBayesModel(DEFAULT_MODEL);
+        } catch (FileNotFoundException e) {
+            return new NaiveBayesModel();
+        }
     }
 
     private Kryo initKryo() {
@@ -75,9 +81,14 @@ public class WSDService {
 
     public Prediction classify(NaiveBayesModel model, IFeatureVector featureVector) {
         return this.classifier.classify(model, featureVector);
+
+    public void serializeNaiveBayesModel() throws FileNotFoundException {
+        assert this.classifier.getModel() instanceof NaiveBayesModel;
+        this.serializeNaiveBayesModel((NaiveBayesModel) this.classifier.getModel(), DEFAULT_MODEL);
     }
 
     public void serializeNaiveBayesModel(NaiveBayesModel model, String path) throws FileNotFoundException {
+        log.info("Serializing Naive Bayes Model to " + path);
         File file = new File(path);
         file.getParentFile().mkdirs();
 
@@ -86,7 +97,12 @@ public class WSDService {
         out.close();
     }
 
+    public NaiveBayesModel deserializeNaiveBayesModel() throws FileNotFoundException {
+        return this.deserializeNaiveBayesModel(DEFAULT_MODEL);
+    }
+
     public NaiveBayesModel deserializeNaiveBayesModel(String path) throws FileNotFoundException {
+        log.info("Deserializing Naive Bayes Model from " + path);
         Input input = new Input(new FileInputStream(path));
         Object model = this.kryo.readClassAndObject(input);
         input.close();
